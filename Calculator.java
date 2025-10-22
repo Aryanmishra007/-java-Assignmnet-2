71377c8ef2c3a82ac792b557722cd7f8f456e9a5
Aryan Mishra <arynmishra2007@gmail.com>
2025-10-22 19:12:03 +0530

    Implement basic calculator operations

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Cannot divide by zero!");
+            return 0;
+        } else {
+            return (double) a / b;
+        }
+    }
+}
